28fe299034196ee1a2bd722d324c38f6f3346b9b
mira-kine <mirak0419@gmail.com>
2021-12-15 10:15:10 -0800

    filters by race

--- src/components/Characters/CharactersList.js
+++ src/components/Characters/CharactersList.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import './Character-List.css';
 
-export default function CharactersList({ chars }) {
+export default function CharactersList({ chars, race, setRace, sortName, setSortName }) {
+  const handleClick = () => {
+    let filteredName = chars.filter((item) => item.name === name || name === 'All');
+    return setSortName(filteredName);
+  };
   return (
     <div>
+      <div className="name-filter-container">
+        <div className="race-filter">
+          <select value={race} onChange={(e) => setRace(e.target.value)}>
+            <option value="All">All</option>
+            <option value="Dwarf">Dwarf</option>
+            <option value="Elf">Elf</option>
+            <option value="Hobbit">Hobbit</option>
+            <option value="Human">Human</option>
+            <option value="Maiar">Maiar</option>
+            <option value="Orc">Orc</option>
+          </select>
+        </div>
+        <button value={sortName} onClick={handleClick}>
+          Filter By Race
+        </button>
+      </div>
       <div className="characters">
         {chars.map((item) => (
-          <div className="char" key={item.id}>
-            <p>{item.name}</p>
+          <div className="char-card" key={item.id}>
+            <h2>{item.name}</h2>
             <p>{item.race}</p>
           </div>
         ))}
--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -5,18 +5,26 @@ import CharactersList from '../../components/Characters/CharactersList';
 
 export default function Characters() {
   const [chars, setChars] = useState([]);
+  const [race, setRace] = useState([]);
+  const [sortName, setSortName] = useState('');
 
   useEffect(() => {
     const getChars = async () => {
-      const response = await fetchCharacters('All');
+      const response = await fetchCharacters(race);
       setChars(response);
     };
     getChars();
-  }, []);
+  }, [race]);
 
   return (
     <div>
-      <CharactersList chars={chars} />
+      <CharactersList
+        chars={chars}
+        setRace={setRace}
+        race={race}
+        sortName={sortName}
+        setSortName={setSortName}
+      />
     </div>
   );
 }
